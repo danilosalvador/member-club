7d418d5e16d2dfab4bd9a855cb974be9f06a3123
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2024-08-04 13:26:02 -0300

    Correção de URL db.json

--- src/services/api-config.js
+++ src/services/api-config.js
@@ -1,6 +1,6 @@
 export const apiConfig = {
     baseURL: process.env.NODE_ENV === "development" 
                 ?  "http://localhost:3333" 
-                : "https://my-json-server.typicode.com/danilosalvador/main-club",
+                : "https://my-json-server.typicode.com/danilosalvador/member-club",
 
 }
